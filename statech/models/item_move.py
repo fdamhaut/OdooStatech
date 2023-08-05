9c38f495ded47d1d18daba3bb35f57855113fec6
Florian Damhaut <flda@odoo.com>
2023-08-05 20:41:15 +0200

    QoL

--- statech/models/item_move.py
+++ statech/models/item_move.py
@@ -7,13 +7,20 @@ class ItemMove(models.Model):
     item_id = fields.Many2one('item', 'Item')
     amount = fields.Float('Amount', default=1)
     chance = fields.Integer('% Chance', default=100)
-    machine_input_id = fields.Many2one('machine', string='Machine')
-    machine_output_id = fields.Many2one('machine', string='Machine')
+    machine_input_id = fields.Many2one('machine')
+    machine_output_id = fields.Many2one('machine')
 
     # Computed
     real_amount = fields.Float(compute='_compute_real_amount', store=True)
+    name = fields.Char(compute="_compute_name")
 
     @api.depends('amount', 'chance')
     def _compute_real_amount(self):
         for im in self:
             im.real_amount = im.amount * im.chance / 100
+
+    @api.depends('amount', 'chance', 'item_id')
+    def _compute_name(self):
+        for im in self:
+            chance = f'{im.chance}% of ' if im.chance < 100 else ''
+            im.name = f'{chance}{im.amount} {im.item_id.name}'
--- statech/models/machine.py
+++ statech/models/machine.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class MachineTag(models.Model):
     _name = 'machine.tag'
@@ -18,6 +18,19 @@ class Machine(models.Model):
     amount = fields.Integer('Amount', default=1)
 
     # Recipe
-    input_ids = fields.One2many('item.move', 'machine_input', 'Inputs')
-    output_ids = fields.One2many('item.move', 'machine_output', 'Outputs')
+    input_ids = fields.One2many('item.move', 'machine_input_id', 'Inputs')
+    output_ids = fields.One2many('item.move', 'machine_output_id', 'Outputs')
     basetime = fields.Integer('Base time')
+
+    input_ids_str = fields.Char(compute='_compute_input_ids_str', string='Inputs')
+    output_ids_str = fields.Char(compute='_compute_output_ids_str', string='Outputs')
+
+    @api.depends('input_ids')
+    def _compute_input_ids_str(self):
+        for machine in self:
+            machine.input_ids_str = ', '.join(machine.input_ids.mapped('name'))
+
+    @api.depends('output_ids')
+    def _compute_output_ids_str(self):
+        for machine in self:
+            machine.output_ids_str = ', '.join(machine.output_ids.mapped('name'))
